e7e765c654291fe23eb22244cc27e5b73bc351ed
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-01 12:34:30 +0500

    WEBSITE: full website prototpe completed

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import (
-    HomeView, Error404View, PrivacyPolicyView, TermsAndConditionsView
+    HomeView, Error404View, PrivacyPolicyView, TermsAndConditionsView,
+    BlogDetailView, BlogView, ContactView, ComingSoonView, LoginView, SignUpView, ForgetPasswordView,
+    SupportView, AboutUsView
 )
 
 app_name = "website"
@@ -10,4 +12,13 @@ urlpatterns = [
     path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy-policy'),
     path('terms-and-conditions/', TermsAndConditionsView.as_view(), name='terms-and-conditions'),
     path('404/', Error404View.as_view(), name='404'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('forget/', ForgetPasswordView.as_view(), name='forget-password'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('support/', SupportView.as_view(), name='support'),
+    path('about/', AboutUsView.as_view(), name='about'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('blog/<slug:slug>/', BlogDetailView.as_view(), name='blog-detail'),
+    path('coming-soon/', ComingSoonView.as_view(), name='coming-soon'),
 ]
--- src/website/views.py
+++ src/website/views.py
@@ -17,5 +17,37 @@ class ContactView(TemplateView):
     template_name = 'website/contact.html'
 
 
+class SupportView(TemplateView):
+    template_name = 'website/support.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'website/about-us.html'
+
+
 class Error404View(TemplateView):
     template_name = 'website/404.html'
+
+
+class BlogView(TemplateView):
+    template_name = 'website/blog.html'
+
+
+class BlogDetailView(TemplateView):
+    template_name = 'website/blog-details.html'
+
+
+class ComingSoonView(TemplateView):
+    template_name = 'website/coming-soon.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'website/sign-in.html'
+
+
+class SignUpView(TemplateView):
+    template_name = 'website/sign-up.html'
+
+
+class ForgetPasswordView(TemplateView):
+    template_name = 'website/forgot.html'
