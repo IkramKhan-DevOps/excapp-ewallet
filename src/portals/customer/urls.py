a68c4e193220807d6809bf7e6bbc0675ebacfe42
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-22 21:58:52 +0500

    List, Detail and Invoicing -> withdrawal, transactiona and topup

--- src/portals/customer/urls.py
+++ src/portals/customer/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import (
-    DashboardView, WalletDetailView
+    DashboardView, WalletDetailView,
+    TopUpListView, TopUpInvoiceView, TopUpDetailView,
+    TransactionListView, TransactionDetailView, TransactionInvoiceView,
+    WithdrawalListView, WithdrawalInvoiceView, WithdrawalDetailView
 )
 
 app_name = "customer-portal"
@@ -8,5 +11,17 @@ urlpatterns = [
 
     path('', DashboardView.as_view(), name='dashboard'),
     path('wallet/', WalletDetailView.as_view(), name='wallet-detail'),
+
+    path('topup/', TopUpListView.as_view(), name='topup-list'),
+    path('topup/1/', TopUpDetailView.as_view(), name='topup-detail'),
+    path('topup/invoice/', TopUpInvoiceView.as_view(), name='topup-invoice'),
+
+    path('transaction/', TransactionListView.as_view(), name='transaction-list'),
+    path('transaction/1/', TransactionDetailView.as_view(), name='transaction-detail'),
+    path('transaction/invoice/', TransactionInvoiceView.as_view(), name='transaction-invoice'),
+
+    path('withdrawal/', WithdrawalListView.as_view(), name='withdrawal-list'),
+    path('withdrawal/1/', WithdrawalDetailView.as_view(), name='withdrawal-detail'),
+    path('withdrawal/invoice/', WithdrawalInvoiceView.as_view(), name='withdrawal-invoice'),
 
 ]
--- src/portals/customer/views.py
+++ src/portals/customer/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from src.accounts.decorators import customer_required
 User = get_user_model()
 
@@ -18,6 +18,64 @@ class DashboardView(TemplateView):
     template_name = 'customer/dashboard.html'
 
 
+""" ---------------------------------------------------------------------------------------------------------------- """
+
+
 @method_decorator(customer_required, name='dispatch')
 class WalletDetailView(TemplateView):
     template_name = 'customer/wallet_detail.html'
+
+
+""" ---------------------------------------------------------------------------------------------------------------- """
+
+
+@method_decorator(customer_required, name='dispatch')
+class TopUpListView(TemplateView):
+    template_name = 'customer/topup_list.html'
+
+
+@method_decorator(customer_required, name='dispatch')
+class TopUpDetailView(TemplateView):
+    template_name = 'customer/topup_detail.html'
+
+
+@method_decorator(customer_required, name='dispatch')
+class TopUpInvoiceView(TemplateView):
+    template_name = 'customer/invoice/topup_invoice.html'
+
+
+""" ---------------------------------------------------------------------------------------------------------------- """
+
+
+@method_decorator(customer_required, name='dispatch')
+class TransactionListView(TemplateView):
+    template_name = 'customer/transaction_list.html'
+
+
+@method_decorator(customer_required, name='dispatch')
+class TransactionDetailView(TemplateView):
+    template_name = 'customer/transaction_detail.html'
+
+
+@method_decorator(customer_required, name='dispatch')
+class TransactionInvoiceView(TemplateView):
+    template_name = 'customer/invoice/transaction_invoice.html'
+
+
+""" ---------------------------------------------------------------------------------------------------------------- """
+
+
+@method_decorator(customer_required, name='dispatch')
+class WithdrawalListView(TemplateView):
+    template_name = 'customer/withdrawal_list.html'
+
+
+@method_decorator(customer_required, name='dispatch')
+class WithdrawalDetailView(TemplateView):
+    template_name = 'customer/withdrawal_detail.html'
+
+
+@method_decorator(customer_required, name='dispatch')
+class WithdrawalInvoiceView(TemplateView):
+    template_name = 'customer/invoice/withdrawal_invoice.html'
+
